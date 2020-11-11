f94914733d72c052405d532b12641ef47b2cb081
chipp972 <pierrecharles.nicolas@gmail.com>
2020-11-11 17:27:08 +0100

    fix: Remove useless files

--- src/features/home/components/FeatureCard.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Card } from '../home.context';
-import { HorizontalCard } from './HorizontalCard';
-import { VerticalCard } from './VerticalCard';
-
-export const FeatureCard: React.FC<Card> = ({ cardType, ...props }) => cardType === 'vertical'
-    ? <VerticalCard {...props} />
-    : <HorizontalCard {...props} />;
